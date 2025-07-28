d465248d9e4258579d1862e2b7faac1c3ac1f84d
Jeremy Cameron <jeremy@cameron.org.au>
2025-07-28 16:57:20 +1000

    Updated framework details dialog

--- components/dialogs/framework-details-dialog.tsx
+++ components/dialogs/framework-details-dialog.tsx
@@ -1,4 +1,5 @@
 import type { DialogProps } from '@radix-ui/react-dialog';
+import { Button } from '../ui/button';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '../ui/dialog';
 
 export default function FrameworkDetailsDialog(props: DialogProps) {
@@ -8,6 +9,19 @@ export default function FrameworkDetailsDialog(props: DialogProps) {
         <DialogHeader>
           <DialogTitle>Framework Details</DialogTitle>
         </DialogHeader>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-1">
+            <h2 className="font-bold text-base">Unsubscribe from framework</h2>
+            <p className="text-muted-foreground text-sm">
+              You will still have access to the framework until the end of your
+              current billing cycle. After that, you will need to re-subscribe
+              to access it again.
+            </p>
+          </div>
+          <Button className="w-fit" variant="destructive">
+            Unsubscribe
+          </Button>
+        </div>
       </DialogContent>
     </Dialog>
   );
